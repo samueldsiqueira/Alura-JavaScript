1dd3aac1acc432a026f479a7766fed70278b53d8
samuel <samueldsiqueira@gmail.com>
2022-02-17 09:45:48 -0300

    exemplos do modulo 1

---
+++ type-string.js
@@ -0,0 +1,25 @@
+const texto1 = "Olá mundo!";
+const texto2 = "Olá mundo!";
+const senha = "senhaSuperSegunda456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Samuel";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// consultar códigos unicodes
+
+const cifrao = "\u0024";
+const aMaiusculo = "\u0041";
+const tique = "\u2705";
+const hiragana = "\u3041";
+
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+
+// template string ou template literal
